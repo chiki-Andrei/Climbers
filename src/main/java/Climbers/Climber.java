a843c12085b3a67706e374767b9422352555422f
andrei <andrei.666.666@mail.ru>
2021-02-27 12:28:02 +0900

    add: createRoute

--- src/main/java/Climbers/Climber.java
+++ src/main/java/Climbers/Climber.java
@@ -2,6 +2,9 @@ package Climbers;
 
 import java.util.Scanner;
 
+import static java.lang.Math.min;
+import static java.lang.Math.random;
+
 public class Climber {
 
     private int stamina;
@@ -22,13 +25,32 @@ public class Climber {
             max_height = 0;
         }
 
-        System.out.println("Введите максимальную крутость подъема:");
+        System.out.println("Введите максимальный перепад ближайших вершин:");
         this.height_difference = input.nextInt();
         if (height_difference < 0){
             height_difference = 0;
         }
     }
 
+    public void createRoute(){
+        int[] route = new int[stamina];
 
+        route[0] = (int) (random()* min(height_difference,max_height)); // ST-10   MH-13   HD- 4    /  4 8
+        System.out.println(route[0]);
+        for(int i = 1; i < stamina; i++){
+            route[i] = getNextPoint(i);
+            System.out.println(route[i]);
+        }
+    }
+
+    private int getNextPoint(int currentPosition){
+        int twoPointDifference = 0;
+
+
+
+        return 0;
+
+
+    }
 
 }
