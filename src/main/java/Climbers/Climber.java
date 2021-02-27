c4d7b7971170e9e1d81e19a0b7f07086655b6022
andrei <andrei.666.666@mail.ru>
2021-02-27 14:08:47 +0900

    edit: getNextPoint

--- src/main/java/Climbers/Climber.java
+++ src/main/java/Climbers/Climber.java
@@ -29,14 +29,18 @@ public class Climber {
         if (height_difference < 0){
             height_difference = 0;
         }
+        else if (height_difference > max_height) {
+            height_difference = max_height;
+        }
+
         input.close();
     }
 
     public void createRoute(){
         int[] route = new int[stamina];
 
-        route[0] = (int) (random()* min(height_difference,max_height)); // ST-10   MH-9   HD- 6
-        route[1] = (int) (random()* min(route[0]+height_difference,max_height));
+        route[0] = (int) (random() * height_difference); // ST-10   MH-9   HD-7
+        route[1] = (int) (random() * min(route[0]+height_difference,max_height));
         System.out.println(route[0]);
         for(int i = 2; i < stamina; i++){
             route[i] = getNextPoint(route[i-2], route[i-1]);
@@ -46,18 +50,21 @@ public class Climber {
 
     private int getNextPoint(int prePrevious, int previous){
         int currentPoint;
-        int twoPastPointsDifference = prePrevious - previous; //2
-        int max_change = height_difference - abs(twoPastPointsDifference);  //4 0-10  сколько могу прибавить к макс из 2-х пред или отнять от мин..
-
+        int twoPastPointsDifference = prePrevious - previous;
+        int max_change = height_difference - abs(twoPastPointsDifference);
+        int a = max(prePrevious, previous) + max_change;
+        int b = min(prePrevious, previous) - max_change;
         if(max(prePrevious, previous)+max_change>max_height){
-            if(min(prePrevious, previous)-max_change<0){
+
+            if(min(prePrevious, previous) - max_change < 0){ // мин точка 0 , макс точка лимит высоты скалолаза
                 currentPoint =1;
-                return currentPoint;
             }
-            else{ // макс точка - лимит высоты скалолаза.
-                currentPoint =1;
-                return currentPoint;
+
+            else{ // макс точка лимит высоты скалолаза
+                currentPoint = (int) (b + Math.random()*(max_height - b)); // +
             }
+
+            return currentPoint;
         }
 
         return 0;
