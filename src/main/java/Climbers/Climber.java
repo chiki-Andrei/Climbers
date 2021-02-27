27b2ccefb25c12aa99e7698354404b7dab429417
andrei <andrei.666.666@mail.ru>
2021-02-27 13:16:57 +0900

    edit: getNextPoint progress

--- src/main/java/Climbers/Climber.java
+++ src/main/java/Climbers/Climber.java
@@ -2,8 +2,7 @@ package Climbers;
 
 import java.util.Scanner;
 
-import static java.lang.Math.min;
-import static java.lang.Math.random;
+import static java.lang.Math.*;
 
 public class Climber {
 
@@ -25,7 +24,7 @@ public class Climber {
             max_height = 0;
         }
 
-        System.out.println("Введите максимальный перепад ближайших вершин:");
+        System.out.println("Введите максимальный перепад трех ближайших вершин:");
         this.height_difference = input.nextInt();
         if (height_difference < 0){
             height_difference = 0;
@@ -36,22 +35,32 @@ public class Climber {
     public void createRoute(){
         int[] route = new int[stamina];
 
-        route[0] = (int) (random()* min(height_difference,max_height)); // ST-10   MH-2   HD- 4    /  2
+        route[0] = (int) (random()* min(height_difference,max_height)); // ST-10   MH-9   HD- 6
         route[1] = (int) (random()* min(route[0]+height_difference,max_height));
         System.out.println(route[0]);
         for(int i = 2; i < stamina; i++){
-            route[i] = getNextPoint(i);
+            route[i] = getNextPoint(route[i-2], route[i-1]);
             System.out.println(route[i]);
         }
     }
 
-    private int getNextPoint(int currentPosition){
-        int twoPastPointsDifference = 0;
+    private int getNextPoint(int prePrevious, int previous){
+        int currentPoint;
+        int twoPastPointsDifference = prePrevious - previous; //2
+        int max_change = height_difference - abs(twoPastPointsDifference);  //4 0-10  сколько могу прибавить к макс из 2-х пред или отнять от мин..
 
-
+        if(max(prePrevious, previous)+max_change>max_height){
+            if(min(prePrevious, previous)-max_change<0){
+                currentPoint =1;
+                return currentPoint;
+            }
+            else{ // макс точка - лимит высоты скалолаза.
+                currentPoint =1;
+                return currentPoint;
+            }
+        }
 
         return 0;
-
 
     }
 
